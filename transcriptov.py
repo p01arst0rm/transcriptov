e78fd5ef3ea93310777966da529113c4f87ac421
polar <polar@ever3st.com>
2018-11-19 22:50:11 +0000

    improved logging & added test transcripts

--- transcriptov.py
+++ transcriptov.py
@@ -4,66 +4,97 @@ import glob
 import tweepy
 import ast
 
+
+
+
 # global variables
 #----------------------------------------------------------------------------
-api_key ="XXXX"
-api_key_secret = "XXXX"   
-access_token = "XXXX"    
-access_token_secret = "XXXX"
-transcript_dir="transcripts/"
+api_key ="XXXXXXXXX"
+api_key_secret = "XXXXXXXXX"   
+access_token = "XXXXXXXXX"    
+access_token_secret = "XXXXXXXXX"
+
+notify_log_file = "./transcriptov.log"
+warn_log_file = "./transcriptov.log"
 err_log_file = "./transcriptov.log"
+
+transcript_dir="transcripts/"
+tweet_state_size=5
+
+
+
 
 # Error handler
 #----------------------------------------------------------------------------
+
+def log_handle(log, log_file):
+        print(log)
+        with open(log_file, "a") as x:
+            x.write(log + "\n")
+   
+def log_notify(notif):
+        log_handle(str("[INFO]: "+notif),notify_log_file)
+
+def log_warn(warn):
+        log_handle(str("[WARNING]: "+warn),warn_log_file)
+
 def log_err(err):
-        print(err)
-        with open(err_log_file, "a") as x:
-            x.write(err + "\n")
-            
+        log_handle(str("[ERROR]: "+err),err_log_file)
+        sys.exit()
+
+
+
+       
 # Twitter handler
 #----------------------------------------------------------------------------
 def get_api():
-        print("[INFO] Loading api..")
+        log_notify("Loading api..")
         auth = tweepy.OAuthHandler(api_key, api_key_secret)
         auth.set_access_token(access_token, access_token_secret)
         api = tweepy.API(auth)
-        print("[INFO] ..api loaded.")
+        log_notify("..api loaded.")
         return api
 
 def send_tweet(tweet):
         api = get_api()
         try:
-                print("[INFO] Sending tweet..")
-                api.update_status(tweet)
-                print("[INFO] ..tweet sent.")
+                log_notify("Sending tweet..")
+                if tweet.strip():
+                        api.update_status(status=tweet)
+                log_notify("..tweet sent.")
         except tweepy.TweepError as e:
                 # format dodgy returns from tweepy api
                 err_dict_list = ast.literal_eval(e.response.text)
                 # print errors
                 for x in err_dict_list['errors']:
-                        err_print = str("[ERROR] {}: {}").format(x['code'],x['message'])
-                        log_err(err_print)
+                        log_err(str("{} ({})").format(x['message'],x['code']))
+
+
+
                        
 # file parsing
 #---------------------------------------------------------------------------- 
 def load_txt_from_dir(file_dir):
+        log_notify("fetching transcripts from \"{}\"..".format(transcript_dir))
         file_list = glob.glob(file_dir + '/*.txt')
         if file_list == []:
-                log_err("[ERROR]: No text files found in transcript directory")
-                sys.exit()
+                log_err(": No text files found in transcript directory")
         else:
                 file_list.sort()
+                log_notify("..Done.")
                 return file_list
 
 def read_file(file_name):
-        print(file_name)
+        log_notify("reading file \"{}\"".format(file_name))
         try:
                 with open(file_name, "r") as f:
                         f=f.read()
                         return f
         except FileNotFoundError:
-                err_print=str("[WARNING]: cannot find file \"{}\"").format(file_name)
-                log_err(err_print)
+                log_warn(str("cannot find file \"{}\"").format(file_name))
+
+
+
 
 # model generation
 #----------------------------------------------------------------------------        
@@ -74,7 +105,7 @@ def gen_model(file_list):
         # generate text models from input files
         while index < num_elements:
                 f = read_file(file_list[index])
-                text_model = markovify.Text(f)
+                text_model = markovify.Text(f, state_size=tweet_state_size)
                 model_list.append(text_model)
                 index+=1
 
@@ -86,6 +117,9 @@ def gen_model(file_list):
         # Only one element, return single element
         return model_list[0]
 
+
+
+
 # main
 #----------------------------------------------------------------------------
 def main(argv):
@@ -94,10 +128,10 @@ def main(argv):
 
         # generate a text model from transcripts
         text_model = gen_model(transcripts)
-
+        
         # generate a tweet from the text model
-        tweet = text_model.make_short_sentence(280)
-
+        tweet = text_model.make_short_sentence(280, tries=100)
+        
         # send generated tweet
         send_tweet(tweet)
 
