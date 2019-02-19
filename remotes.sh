48aa18c424c0c4601fe8d9c8ef2427550b953e09
p01arst0rm <polar@ever3st.com>
2019-02-19 01:07:34 +0000

    updated remotes

--- remotes.sh
+++ remotes.sh
@@ -1,3 +1,4 @@
-git remote add github https://github.com/p01arst0rm/transcriptov
+git remote remove origin
+git remote add github https://github.com/p01arst0rm/transcriptov/
 git remote add gitlab https://gitlab.com/p01arst0rm/transcriptov.git
 
